4df04511368f30e9e19214e519feef5ed1ce53a0
Tiffany Robbins-Cabell <lillialskye@yahoo.com>
2018-06-26 19:01:03 -0400

    need to migrate bookmarks and fix URL

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Note, PersonalNote, Bookmarks
 
 # Register your models here.
+admin.site.register(Note)
+admin.site.register(PersonalNote)
+admin.site.register(Bookmarks)
--- notes/models.py
+++ notes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 
 class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable= False)
@@ -8,8 +9,18 @@ class Note(models.Model):
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
-    #TODO Tagging system or categories
+    
 
-class Bookmark(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable= False)
-    url= models    
+    
+    #TODO Tagging system or categories
+    category = models.CharField(max_length=20)
+
+
+class PersonalNote(Note):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+   
+class Bookmarks(models.Model):
+   id = models.UUIDField(primary_key=True, default=uuid4, editable= False)
+  # url = models.UrlField(max_length=200)    
+   title = models.CharField(max_length=100)
+   date_added = models.DateTimeField(auto_now=True)
